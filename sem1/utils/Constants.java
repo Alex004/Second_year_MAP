168a769c49fd8f6c7b0dea4afbce530fa427d98c
Alex004 <pungaalexandru@yahoo.com>
2020-04-01 13:05:54 +0300

    Delete Constants.java

--- sem1/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package sem1.utils;
-
-import java.time.format.DateTimeFormatter;
-
-public class Constants {
-    public static final DateTimeFormatter DATE_TIME_FORMATER=DateTimeFormatter.ofPattern("YYYY-MM-dd hh:mm");
-
-}
