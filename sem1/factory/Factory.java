445c4fcb34813d1a4638b5ba415972c307f1f943
Alex004 <pungaalexandru@yahoo.com>
2020-04-01 13:06:45 +0300

    Delete Factory.java

--- sem1/factory/Factory.java
+++
@@ -1,8 +0,0 @@
-package sem1.factory;
-
-import sem1.containers.Container;
-import sem1.containers.Strategy;
-
-public interface Factory {
-    public Container createContainer(Strategy strategy);
-}
